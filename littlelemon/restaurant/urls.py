e91550e77d3dcf75c3e22986f62e648d05bce6ed
kay438 <philokay10@gmail.com>
2023-12-13 19:05:11 +0300

    To update Templates and static

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib import admin
-from .views import sayHello
+from . import views
 
-urlpatterns=[
-    path('', sayHello, name="sayHello"),
+urlpatterns = [
+    path('', views.index, name="index"),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#from . import templates
 # Create your views here.
 
-def sayHello(request):
-    return HttpResponse ("Hello World")
+def index(request):
+    return render(request, 'index.html', {})
